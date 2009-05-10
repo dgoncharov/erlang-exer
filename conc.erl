6cc379ead4c3956611939cec8d7af52e6e320041
Dmitry Goncharov <gonch.dmitry@gmail.com>
2009-05-10 19:11:00 +0400

    ref

--- conc.erl
+++ conc.erl
@@ -8,31 +8,32 @@ main([A]) ->
 	init:stop().
 
 conc(File) ->
-	{ok, S} = file:open(File, read),
-	process_line(io:get_line(S, ''), [], 1, S).
-
+	{ok, FD} = file:open(File, read),
+	process_line(io:get_line(FD, ''), [], 1, FD).
 
 process_line(eof, Result, _, _) ->
 	Result;
 
-process_line(String, Result, Line, S) ->
-		R1 = string:tokens(String, "<>\|/+-=~^!@#$%&*()[]{}?:,. ;\n\t\v\"\'"),
-		R2 = process_words(R1, Result, Line),
-		R3 = process_line(io:get_line(S, ''), R2, Line + 1, S),
-		R3.
+process_line(String, Result, Line, FD) ->
+	R1 = string:tokens(String, "<>\|/+-=~^!@#$%&*()[]{}?:,. ;\n\t\v\"\'"),
+	R2 = process_words(R1, Result, Line),
+	R3 = process_line(io:get_line(FD, ''), R2, Line + 1, FD),
+	R3.
 
-process_words([H|T], Result, Line) ->
+process_words([H|_T]=L, Result, Line) ->
 	R = lists:keyfind({H, Line}, 1, Result),
-	if R =:= false ->
-		R1 = lists:append(Result, [{{H, Line}, 1}]),
-		process_words(T, R1, Line);
-	true ->
-                R2 = [{{W, Line2}, N2} || {{W, Line2}, N2} <- Result, (W =/= H) or (Line2 =/= Line)],
-		{{W, _}, N2} = R, %TODO: read the line and assert it is equal to Line
-		R3 = lists:append(R2, [{{W, Line}, N2 + 1}]),
-		process_words(T, R3, Line)
-	end;
+	process_word(R, L, Result, Line);
 
 process_words([], Result, _) ->
 	Result.
+
+process_word(false, [H|T], Result, Line) ->
+	R = lists:append(Result, [{{H, Line}, 1}]),
+	process_words(T, R, Line);
+
+process_word(Tuple, [H|T], Result, Line) ->
+        R = [{{W, Line2}, N} || {{W, Line2}, N} <- Result, (W =/= H) or (Line2 =/= Line)],
+	{{W, _Line}, N} = Tuple, %TODO: read the line and assert it is equal to Line
+	R1 = lists:append(R, [{{W, Line}, N + 1}]),
+	process_words(T, R1, Line).
 
