e90b1c0295aad81a3a1765fbf6273a7d089ab5c7
Dmitry Goncharov <gonch.dmitry@gmail.com>
2009-05-10 18:29:57 +0400

    ref

--- conc.erl
+++ conc.erl
@@ -17,24 +17,23 @@ read(Result, Line, S) ->
 		Result;
 	true ->
 		R1 = string:tokens(R, "<>\|/+-=~^!@#$%&*()[]{}?:,. ;\n\t\v\"\'"),
-		R2 = process_line(R1, Result, Line),
+		R2 = process_words(R1, Result, Line),
 		R3 = read(R2, Line + 1, S),
 		R3
 	end.
 
-
-process_line([H|T], Result, Line) ->
+process_words([H|T], Result, Line) ->
 	R = lists:keyfind({H, Line}, 1, Result),
 	if R =:= false ->
 		R1 = lists:append(Result, [{{H, Line}, 1}]),
-		process_line(T, R1, Line);
+		process_words(T, R1, Line);
 	true ->
                 R2 = [{{W, Line2}, N2} || {{W, Line2}, N2} <- Result, (W =/= H) or (Line2 =/= Line)],
 		{{W, _}, N2} = R, %TODO: read the line and assert it is equal to Line
 		R3 = lists:append(R2, [{{W, Line}, N2 + 1}]),
-		process_line(T, R3, Line)
+		process_words(T, R3, Line)
 	end;
 
-process_line([], Result, _) ->
+process_words([], Result, _) ->
 	Result.
 
