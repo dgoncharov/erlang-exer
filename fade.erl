8d65a8b704901bc8e139bc9716bd7d55327e479b
Dmitry Goncharov <gonch.dmitry@gmail.com>
2009-05-11 18:39:40 +0400

    fade.erl added under source control

---
+++ fade.erl
@@ -0,0 +1,15 @@
+#!/usr/bin/env escript
+
+%% This script decolorizes files by removing color related escape sequences.
+
+main([Infile, Outfile]) ->
+	fade(Infile, Outfile);
+
+main(_) ->
+	io:format("usage: fade.erl infile outfile~n").
+
+fade(Infile, Outfile) ->
+	{ok, D} = file:read_file(Infile),
+	P = re:replace(D, "\033\\[(1;)?\\d{0,2}m", "", [{return, list}, global]),
+	file:write_file(Outfile, P).
+
